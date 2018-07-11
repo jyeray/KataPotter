9fde8c84f4eab133769ed64c9b810d202b8528a7
Juan Antonio <juanantonio19344@hotmail.com>
2018-07-11 19:48:47 +0100

    when you buy three differents books

--- src/library.js
+++ src/library.js
@@ -1,16 +1,20 @@
 module.exports = function Library() {
     function buy(books) {        
-        return books.length * 8 * getDiscount(books);
+        return books.length * 8 * (1 - getDiscount(books));
     }
     
     function getDiscount(books) {
-        return books.length == 2 && books[0].id !== books[1].id ? 0.95 : 1
+        const numberOfBooks = books.reduce((acc, book) => {
+            const id = book.id;
+            acc[id] = acc.id + 1;
+            return acc;
+        }, {});
+        return (Object.keys(numberOfBooks).length - 1) * 0.05;
     }
     
     return {
         buy
     }
-    
 }
 
 
--- test/index.test.js
+++ test/index.test.js
@@ -19,8 +19,8 @@ describe('library', () => {
         expect(Library().buy([Book(1), Book(2)])).toBe(15.2);
     });
 
-    it('when you buy three identical books', () => {
-        expect(Library().buy([Book(1), Book(1), Book(1)])).toBe(21.6);
+    it('when you buy three differents books', () => {
+        expect(Library().buy([Book(1), Book(2), Book(4)])).toBe(21.6);
     });
 }) 
 
