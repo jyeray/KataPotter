7f3bcbc5aebce0c5dfbdad144c65ec12411d3361
Juan Antonio <juanantonio19344@hotmail.com>
2018-07-11 19:23:56 +0100

    when you buy two different books

--- test/index.test.js
+++ test/index.test.js
@@ -6,13 +6,18 @@ describe('library', () => {
     it('when you buy zero books', () => {
         expect(Library().buy([])).toBe(0);
     });
+
     it('when you buy one book', () => {
         expect(Library().buy([Book(1)])).toBe(8);
     });
+
     it('when you buy two identical books', () => {
         expect(Library().buy([Book(1), Book(1)])).toBe(15.2);
     });
-    
+
+    it('when you buy two different books', () => {
+        expect(Library().buy([Book(1), Book(2)])).toBe(16);
+    });
 
 }) 
 
